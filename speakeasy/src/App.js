a123a5b235e36eec3f11b896b5e5b8c23a22f082
J. Smith <joe.a.smith@gmail.com>
2021-01-24 15:42:33 -0600

    clean button

--- speakeasy/src/App.js
+++ speakeasy/src/App.js
@@ -7,11 +7,11 @@ import Logout from './components/Logout';
 function App() {
   return (
     <div className="App">
-      <h2>Garbar A Speakeasy</h2>
+      <h2>Welcome to Garbar A Speakeasy Please login</h2>
       <Login />
       <br />
       <Logout />
-      <h2>The Hooks way</h2>
+      <h2>For some great BBQ</h2>
   
     </div>
   );
